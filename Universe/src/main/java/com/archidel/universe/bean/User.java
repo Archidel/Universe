6618bfe528cae1039d0abbc923b241e0f86db04b
Albert <albert_zarankovich@epam.com>
2017-11-20 13:14:22 +0300

    my seccond commit

--- Universe/src/main/java/com/archidel/universe/bean/User.java
+++ Universe/src/main/java/com/archidel/universe/bean/User.java
@@ -15,8 +15,7 @@ import javax.persistence.Table;
 public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
 
-	private String myString;
-	
+	private String lol;
 	
 	@Id
 	@GeneratedValue
